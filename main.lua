81a4509974b39f2f88fccec319982746cf9f0a79
Sergey Vasyunin <sgoodsan@gmail.com>
2016-06-19 22:59:16 +0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,12 +4,6 @@
 --
 ---------------------------------------------------------------------------------
 
--- hide the status bar
---display.setStatusBar( display.HiddenStatusBar )
-
---local system = require("system")
---system.setTapDelay( 5 )
-
 -- require the composer library
 local composer = require "composer"
 
